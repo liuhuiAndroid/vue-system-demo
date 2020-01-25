349d59b1bb174251960afd3e610e6577d7e3c5d1
liu hui <im.lh@hotmail.com>
2020-01-25 14:36:46 +0800

    登录交互实现

--- vue-mall/src/main.js
+++ vue-mall/src/main.js
@@ -3,6 +3,7 @@ import router from "./router"
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueLazyLoad from 'vue-lazyload'
+import VueCookie from 'vue-cookie'
 import App from './App.vue'
 // import env from './env'
 
@@ -27,9 +28,12 @@ axios.interceptors.response.use(function (response) {
         window.location.href = '/#/login';
     } else {
         alert(res.msg);
+        // 抛出异常
+        return Promise.reject(res);
     }
 });
 Vue.use(VueAxios, axios);
+Vue.use(VueCookie);
 Vue.use(VueLazyLoad,{
     loading:'/imgs/loading-svg/loading-bars.svg'
 })
