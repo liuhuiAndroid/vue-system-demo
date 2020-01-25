06f4ac7aa1f101b8733df962961fc317185fc696
liu hui <im.lh@hotmail.com>
2020-01-25 20:39:03 +0800

    Vuex 框架搭建

---
+++ vue-mall/src/store/index.js
@@ -0,0 +1,21 @@
+/**
+ * @作者 lh
+ * @创建时间 2020/1/25 20:24
+ * 管理入口
+ */
+import Vue from 'vue'
+import Vuex from 'vuex'
+import mutations from './mutations'
+import actions from './actions'
+
+Vue.use(Vuex);
+
+const state = {
+    username: '', // 登录用户名
+    cartCount: 0, // 购物车商品数量
+}
+export default new Vuex.Store({
+    state,
+    mutations,
+    actions
+})
---
+++ vue-mall/src/store/mutations.js
@@ -0,0 +1,8 @@
+/**
+ * @作者 lh
+ * @创建时间 2020/1/25 20:24
+ * 商城Vuex-mutations
+ */
+export default {
+
+}
