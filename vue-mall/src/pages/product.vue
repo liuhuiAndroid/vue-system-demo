da00ba250b65c1f22b61c7714ea1d7e563cb913d
liu hui <im.lh@hotmail.com>
2020-01-26 00:06:07 +0800

    产品站参数组件实现

--- vue-mall/src/pages/product.vue
+++ vue-mall/src/pages/product.vue
@@ -1,15 +1,28 @@
 <template>
-    <div>
-        product
+    <div class="product">
+        <product-param  title="小米9">
+            <template v-slot:buy>
+                <button class="btn">立即购买</button>
+            </template>
+        </product-param>
     </div>
 </template>
 
 <script>
+    import ProductParam from './../components/ProductParam'
+
     export default {
-        name: "product"
+        name: "product",
+        components: {
+            ProductParam
+        }
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+    .product {
+        button {
+            margin-left: 10px;
+        }
+    }
 </style>
