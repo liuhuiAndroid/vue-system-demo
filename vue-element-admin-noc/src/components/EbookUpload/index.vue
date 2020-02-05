5aabd1e59be673c8a64232cbe15f68b62fd0a466
liu hui <im.lh@hotmail.com>
2020-02-05 12:15:52 +0800

    上传组件开发

--- vue-element-admin-noc/src/components/EbookUpload/index.vue
+++ vue-element-admin-noc/src/components/EbookUpload/index.vue
@@ -1,7 +1,29 @@
 <template>
-    <div class="upload-container">
-      <el-upload :action="action" :headers="headers">请上传</el-upload>
-    </div>
+  <div class="upload-container">
+    <el-upload
+      :action="action"
+      :headers="headers"
+      :multiple="false"
+      :limit="1"
+      :before-upload="beforeUpload"
+      :on-success="onSuccess"
+      :on-error="onError"
+      :on-remove="onRemove"
+      :file-list="fileList"
+      :on-exceed="onExceed"
+      :disabled="disabled"
+      drag
+      show-file-list
+      accept="application/epub+zip"
+      class="image-upload"
+    >
+      <i class="el-icon-upload"/>
+      <div class="el-upload__text" v-if="fileList.length === 0">
+        请将电子书拖入或<em>点击上传</em>
+      </div>
+      <div class="el-upload__text" v-else>图书已上传</div>
+    </el-upload>
+  </div>
 </template>
 
 <script>
@@ -9,16 +31,60 @@
 
     export default {
         name: "index",
-        data(){
+        props: {
+            fileList: {
+                type: Array,
+                default() {
+                    return []
+                }
+            },
+            disabled: {
+                type: Boolean,
+                default: false
+            }
+        },
+        data() {
             return {
                 action: `${process.env.VUE_APP_BASE_API}/book/upload`
             }
         },
         computed: {
-            headers(){
+            headers() {
                 return {
                     Authorization: `Bearer ${getToken()}`
                 }
+            }
+        },
+        methods: {
+            beforeUpload(file) {
+                console.log(file)
+                this.$emit('beforeUpload', file)
+            },
+            onSuccess() {
+
+            },
+            onError(err) {
+                const errMsg = (err.message && JSON.parse(err.message))
+                this.$message({
+                    message: (errMsg && errMsg.msg && `上传失败，失败原因：${errMsg.msg}`) || '上传失败',
+                    type: 'error'
+                })
+                this.$emit('onError', err)
+            },
+            onRemove() {
+
+            },
+            fileList() {
+
+            },
+            onExceed() {
+                this.$message({
+                    message: '每次只能上传一本电子书',
+                    type: 'warning'
+                })
+            },
+            onExceed() {
+
             }
         }
     }
